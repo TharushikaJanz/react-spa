2436ae398db0d662cdf0a7b65461851c85cbc109
tharushi <tharushika@sutratechnologies.com>
2023-04-03 09:25:27 +0530

    UserView | HomePage created

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import ProfileCard from './sub-components/ProfileCard'
+import { Box, Grid, Typography } from '@mui/material';
 
 function UserView() {
     const [usersData, setUsersData] = useState([]);
 
     useEffect(() => {
-      fetch('https://reqres.in/api/users?page=1')
-        .then(response => response.json())
-        .then(data => setUsersData(data.data))
-        .catch(error => console.log(error));
+        fetch('https://reqres.in/api/users?page=1')
+            .then(response => response.json())
+            .then(data => setUsersData(data.data))
+            .catch(error => console.log(error));
     }, []);
-  
-    console.log(usersData)
+
     return (
-        <div>
-            <h1>UserView</h1>
-            {usersData.map((user) => (
-                <ProfileCard firstName={user.first_name} email={user.email}/>
-            ))}
-            
-        </div>
+        <>
+            <Box
+                sx={{
+                    backgroundColor: "#60ADC2",
+                    padding: 2,
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                <Typography variant="h5" color="#fff"><b>Home Page</b></Typography>
+            </Box>
+            <Box
+                sx={{
+                    padding: 5,
+                    display: 'flex',
+                    justifyContent: 'left'
+                }}>
+                <Typography variant="h5" color="#000"><b>Users</b></Typography>
+            </Box>
+
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Grid container spacing={0} style={{ width: '100%', maxWidth: '900px' }}>
+                    {usersData.map((user, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index} style={{ padding: 2 }}>
+                            <ProfileCard userAvatar={user.avatar} firstName={user.first_name} email={user.email} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
+
+
+        </>
     )
 }
 
--- src/components/sub-components/ProfileCard.js
+++ src/components/sub-components/ProfileCard.js
@@ -1,25 +1,46 @@
-import { Avatar, Paper } from '@mui/material'
+import { Avatar, Box, Paper, Typography } from '@mui/material'
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function ProfileCard(props) {
-    const { firstName, email } = props
+    const { firstName, email, userAvatar } = props
     return (
-        <Paper elevation={2} sx={{
-            width: '190px', display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            padding: 1,
-        }}>
+        <Paper elevation={2}
+            component="button"
+            sx={{
+                width: '200px', display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 1,
+            }}
+            onClick={() => console.log("Pressed")}>
             <Avatar
                 alt="Profile Icon"
                 variant="square"
-                style={{ width: '185px', height: '185px', backgroundColor: '#EEF5F5', borderRadius: 5, borderColor: '#fff', border: '1px solid black' }}
+                style={{
+                    width: '185px',
+                    height: '185px',
+                    backgroundColor: '#EEF5F5',
+                    borderRadius: 5,
+                    borderColor: '#fff',
+                    border: '1px solid black',
+                }}
             >
-                <AccountCircleIcon sx={{ fontSize: 210, color: "#92D0E1" }} />
+                <img src={userAvatar} alt={firstName} />
             </Avatar>
-            <div>{firstName}</div>
-            <div>{email}</div>
+            <div>
+                <Box>
+                    <Typography variant="body1" align='left'>
+                        <b>{firstName}</b>
+                    </Typography>
+                </Box>
+                <Box >
+                    <Typography variant="body2">
+                        {email}
+                    </Typography>
+                </Box>
+            </div>
         </Paper>
 
 
